962a92e3208efbe4874b887c07ef9360d8c77898
juansacristan <juanenciso1007@gmail.com>
2022-07-20 17:30:26 -0500

    argegue unos archivos en carpetas y menu

--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -9,3 +9,19 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+  close.addEventListener('click', () => {
+      nav.classList.remove('active');
+  })
+}
