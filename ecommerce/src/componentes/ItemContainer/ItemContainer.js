cc0bf74292ea91b7e82cba7e0b50147f8c7be6fb
juansacristan <juanenciso1007@gmail.com>
2022-08-02 13:00:49 -0500

    agregue el codigo parte componentes 2

--- ecommerce/src/componentes/ItemContainer/ItemContainer.js
+++ ecommerce/src/componentes/ItemContainer/ItemContainer.js
@@ -2,16 +2,52 @@ import ItemProduct from "../ItemProduct/ItemProduct"
 import './ItemContainer.css'
 
 const ItemContainer = ({section}) =>{
-
+    const product1 = {
+        title: "Victory bomber 150",
+        price: 5999000,
+        image: 'Victory-bomber150-01.png',
+        stock: 8,
+    }
+    const product2 = {
+        title: "Victory switch",
+        price: 6799000,
+        image: 'Victory-switch.png',
+        stock: 10,
+    }
+    const product3 = {
+        title: "Victory bomber 250",
+        price: 5999000,
+        image: 'Victory-bomber-250.png',
+        stock: 4,
+    }
+    const product4 = {
+        title: "Benelli TNT 135",
+        price: 9499000,
+        image: 'Benelli-tnt135.png',
+        stock: 7,
+    }
+    const product5 = {
+        title: "Benelli 180s",
+        price: 10799000,
+        image: 'Benelli-180s.png',
+        stock: 10,
+    }
+    const product6 = {
+        title: "Benelli TNT23N",
+        price: 12799000,
+        image: 'Benelli-tnt25n.png',
+        stock: 8,
+    }
+    const handleSubmit = () => {console.log("Envio de info")}
 return(
     <div className='list-products'>
         <h2>{section}</h2>
-        <ItemProduct title="Victory bomber 150" price="$ 5.999.000" image={'Victory-bomber150-01.png'}/>
-        <ItemProduct title="Victory switch" price="$ 6.799.000" image={'Victory-switch.png'}/>
-        <ItemProduct title="Victory bomber 250" price="$ 10.990.000" image={'Victory-bomber-250.png'}/>
-        <ItemProduct title="Benelli TNT 135" price="$ 9.499.000" image={'Benelli-tnt135.png'}/>
-        <ItemProduct title="Benelli 180s" price="$ 10.799.000" image={'Benelli-180s.png'}/>
-        <ItemProduct title="Benelli TNT23N" price="$ 12.799.000" image={'Benelli-tnt25n.png'}/>
+        <ItemProduct data={product1} action={handleSubmit}/>
+        <ItemProduct data={product2}/>
+        <ItemProduct data={product3}/>
+        <ItemProduct data={product4}/>
+        <ItemProduct data={product5}/>
+        <ItemProduct data={product6}/>
     </div>
 )
 }
--- ecommerce/src/componentes/ItemProduct/ItemProduct.js
+++ ecommerce/src/componentes/ItemProduct/ItemProduct.js
@@ -1,12 +1,52 @@
+import { useEffect, useState } from 'react'
 import './ItemProduct.css'
-const ItemProduct = ({title, price, image}) => {
+
+const ItemProduct = ({data, action}) => {
+    const [contador, setContador] = useState(0)
+
+    const {title, image, price, stock} = data
+
+    const sumaNum = () =>{
+        setContador(contador + 1)
+    }
+    const restaNum = () =>{
+        setContador(contador - 1)
+    }
+
+    // 1. Mount -> Crear
+    
+    // useEffect(() =>{
+    // }, [])
+
+    // 2. Update/Change/Actualizacion -> Cambio el valor de State/prop
+    
+    // useEffect(() =>{
+    // })
+
+    // 3. Unmount/Desmontaje -> Destruye/Desaperece
+    
+    // useEffect(() =>{
+        // return () =>{
+        //   console.log("Se desmonta el componente")
+        // }
+    // }, [])
+
+    useEffect(() =>{
+        console.log("Actualizacion")
+    }, [])
 
     return (
         <div className="item-product">
             <img src={`/assets/${image}`} alt ="Imagen producto"/>
             <p>{title}</p>
-            <span>{price}</span>
-            <button>Comprar</button>
+            <span>$ {price} COP</span>
+            <p>stock: {stock}</p>
+            <div className='countProd'>
+                <button onClick={restaNum}>-</button>
+                <p>{contador}</p>
+                <button onClick={sumaNum}>+</button>
+            </div>
+            <button onClick={action}>Comprar</button>
         </div>
     )
 }
